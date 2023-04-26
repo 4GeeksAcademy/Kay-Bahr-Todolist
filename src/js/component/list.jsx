5d39a57918649baebbefc79cec14c65948ddbb4a
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-26 06:29:42 +0000

    structure commit

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,21 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Footer from "./footer.jsx";
 
 const List = () => {
-	const [inputValue, setInputValue ] = useState('');
-    
-    const validateInput = () => {
-		if(inputValue === "") alert("The input cannot be empty")
-	}
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState([]);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
 
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">todos</h1>
-    		<div>
-        		<input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} />
-        		<button onClick={validateInput} />
-    		</div>
-		</div>
-	);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setItems([...items, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const itemCount = items.length;
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(-1);
+  };
+
+  const handleRemove = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
+  return (
+    <div className="text-center">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Thasadith" />
+      </head>
+      <h1 className="text-center mt-5">todos</h1>
+      <div className="toDoList">
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          value={inputValue}
+        />
+        <ul className="list">
+          {items.map((item, index) => (
+            <li key={index} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}>
+              {item}
+              {hoveredIndex === index && (
+                <button className="closer" onClick={() => handleRemove(index)}>
+                  x
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+        <Footer itemCount={itemCount} />
+      </div>
+    </div>
+  );
 };
 
 export default List;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,8 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
 import List from "./component/list.jsx";
 
-//render your react application
 ReactDOM.render(<List />, document.querySelector("#app"));
