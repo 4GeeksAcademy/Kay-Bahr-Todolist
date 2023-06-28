d92e05513dcaf5fc70e383f8f782f9ac99e2595a
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-06-28 03:43:38 +0000

    ftch commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,19 @@ import "../styles/index.css";
 
 import List from "./component/list.jsx";
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Add the cors middleware
+app.use(cors());
+
+// Rest of your code...
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 ReactDOM.render(<List />, document.querySelector("#app"));
